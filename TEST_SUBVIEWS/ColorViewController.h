2e707182a4a60d0443688cdf09fe3e5376b03c4a
Colas <colasjojo@yahoo.fr>
2015-04-30 18:32:38 +0200

    first commit

---
+++ TEST_SUBVIEWS/ColorViewController.h
@@ -0,0 +1,13 @@
+//
+//  ColorViewController.h
+//  CREATION_NAVIGATION_CONTROLLER
+//
+//  Created by Colas on 30/04/2015.
+//  Copyright (c) 2015 cassiopeia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ColorViewController : UIViewController
+
+@end
---
+++ TEST_SUBVIEWS/ColorViewController.m
@@ -0,0 +1,44 @@
+//
+//  ColorViewController.m
+//  CREATION_NAVIGATION_CONTROLLER
+//
+//  Created by Colas on 30/04/2015.
+//  Copyright (c) 2015 cassiopeia. All rights reserved.
+//
+
+#import "ColorViewController.h"
+
+@interface ColorViewController ()
+
+@end
+
+@implementation ColorViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+    CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
+    CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
+    CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
+    UIColor *color = [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
+    
+    self.view.backgroundColor = color ;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
